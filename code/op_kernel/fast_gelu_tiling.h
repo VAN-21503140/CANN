47e8e5a327b38f321554d542bdfa8fd34d68ea3e
VAN-21503140 <930764273@qq.com>
2026-07-07 22:30:58 +0800

    Apply V3 simple tiling
    CANNJudge submission 85014: Pass 5/5, times 4.18us, 3.76us, 8.06us, 7.32us, 9.62us; elapsed about 66 seconds.

--- code/op_kernel/fast_gelu_tiling.h
+++ code/op_kernel/fast_gelu_tiling.h
@@ -1,16 +1,8 @@
-// Host-to-kernel tiling contract.
+// Tiling结构体定义的头文件
 #pragma once
 
 #include <cstdint>
 
 struct FastGeluTilingData {
-    uint32_t totalLength;
-    uint32_t smallCoreDataNum;
-    uint32_t bigCoreDataNum;
-    uint32_t finalBigTileNum;
-    uint32_t finalSmallTileNum;
-    uint32_t tileDataNum;
-    uint32_t smallTailDataNum;
-    uint32_t bigTailDataNum;
-    uint32_t tailBlockNum;
+    uint32_t length;
 };
